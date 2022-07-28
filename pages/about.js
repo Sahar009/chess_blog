558e346610b0e339e1edc524ddefe6a588214764
Akinwumi sehinde <akinwumisehinde@gmail.com>
2022-07-28 08:16:51 +0100

    New Pages and css

---
+++ pages/about.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Footer } from '../components/footer'
+import { Toolbar } from '../components/toolbar'
+
+const about = () => {
+  return (
+    <div >
+      <Toolbar/>
+      <div className='about'>
+      
+      
+        <h4>Shepredict is a sports prediction website that offers football predictions, tennis predictions, basketball predictions, handball predictions, ice hockey predictions, tips & analysis based on algorithms, detailed analysis, betting tips, forms and statistics, and the most recent results for a wide range of sports.<br/>
+   
+    By giving valuable bets on athletic events, particularly soccer predictions, we are devoted to achieving maximum earnings and returns for you. Every day of the week, Monday through Sunday, we give you with the most accurate and guaranteed sport predictions. <br/>
+    We are unrelenting in our efforts to help as many bettors as possible benefit from the recommendations we give.
+    
+    shepredict's specialists provide predictions for over 50 leagues, including football, tennis, basketball, handball, and ice hockey. We are meticulous in our work and are focused on the success of our customers. We pledge to assist you with your wagers!
+    </h4>
+    </div>
+    
+    <Footer/>
+    </div>
+    
+  )
+}
+
+export default about
