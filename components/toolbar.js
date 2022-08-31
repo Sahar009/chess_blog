da1c0889a3c20ed938672f30207a0d44412f49f1
Akinwumi sehinde <akinwumisehinde@gmail.com>
2022-08-31 10:03:24 +0100

    latest commit
    css & animation AOS

--- components/toolbar.js
+++ components/toolbar.js
@@ -2,16 +2,25 @@ import { useRouter } from 'next/router';
 import Link from "next/link";
 import styles from '../styles/Toolbar.module.css';
 // import styles from '../styles/styleList.module.scss'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 
 export const Toolbar = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
   const router = useRouter();
   const [activeToolbar, setActiveToolbar] = useState('#')
+ 
   return (
-    <div className={styles.main}>
-      <div className={activeToolbar === '#' ? 'active' : ''} onClick={() => router.push('/')}>Home</div>
-      <div onClick={() => router.push('contact')} className={activeToolbar === '#contact' ? 'active' : ''}>Contact</div>
-      <div onClick={() => router.push('about')}className={activeToolbar === '#about' ? 'active' : ''}>About</div>
+    <div className={styles.main} >
+      <div className={activeToolbar === '#' ? 'active' : ''} onClick={() => router.push('/')} data-aos="fade-left">Home</div>
+      <div onClick={() => router.push('contact')} className={activeToolbar === '/contact' ? 'active' : ''} data-aos="fade-in">Contact</div>
+      <div onClick={() => router.push('about')}className={activeToolbar === '#about' ? 'active' : ''} data-aos="fade-right">About</div>
       
 
     </div>
